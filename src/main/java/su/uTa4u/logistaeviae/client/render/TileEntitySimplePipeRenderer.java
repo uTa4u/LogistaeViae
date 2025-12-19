33a74f6b9e8eb759322bdb6b85d6e626112f519d
uTa4u <63360499+uTa4u@users.noreply.github.com>
2025-12-19 23:18:00 +0300

    flying apple sadge

--- src/main/java/su/uTa4u/logistaeviae/client/render/TileEntitySimplePipeRenderer.java
+++ src/main/java/su/uTa4u/logistaeviae/client/render/TileEntitySimplePipeRenderer.java
@@ -9,11 +9,13 @@ import net.minecraft.client.renderer.block.model.SimpleBakedModel;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.client.renderer.vertex.VertexFormat;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.client.MinecraftForgeClient;
 import net.minecraftforge.client.model.BakedItemModel;
@@ -92,33 +94,37 @@ public final class TileEntitySimplePipeRenderer extends FastTESR<TileEntitySimpl
         TextureAtlasSprite tex = quad.getSprite();
         int[] vertexData = quad.getVertexData();
 
-        final double size = 0.375;
-        final double start = 0.3125;
-        final double zStart = 0.5;
+        EntityPlayer player = Minecraft.getMinecraft().player;
+        float pitch = player.rotationPitch;
+        float yaw = player.rotationYaw;
+
+        double start = 0.25;
+        double end = start + 0.5;
+        double zStart = 0.5;
 
         int offset = 0;
-        buffer.pos(start, start, zStart);
+        bufferPos(buffer, start, start, zStart, pitch, yaw);
         bufferColor(buffer, vertexData[3 + offset]);
         buffer.tex(tex.getMinU(), tex.getMaxV());
         buffer.lightmap(skyLight, blockLight);
         buffer.endVertex();
 
         offset += 7;
-        buffer.pos(start + size, start, zStart);
+        bufferPos(buffer, end, start, zStart, pitch, yaw);
         bufferColor(buffer, vertexData[3 + offset]);
         buffer.tex(tex.getMaxU(), tex.getMaxV());
         buffer.lightmap(skyLight, blockLight);
         buffer.endVertex();
 
         offset += 7;
-        buffer.pos(start + size, start + size, zStart);
+        bufferPos(buffer, end, end, zStart, pitch, yaw);
         bufferColor(buffer, vertexData[3 + offset]);
         buffer.tex(tex.getMaxU(), tex.getMinV());
         buffer.lightmap(skyLight, blockLight);
         buffer.endVertex();
 
         offset += 7;
-        buffer.pos(start, start + size, zStart);
+        bufferPos(buffer, start, end, zStart, pitch, yaw);
         bufferColor(buffer, vertexData[3 + offset]);
         buffer.tex(tex.getMinU(), tex.getMinV());
         buffer.lightmap(skyLight, blockLight);
@@ -128,6 +134,24 @@ public final class TileEntitySimplePipeRenderer extends FastTESR<TileEntitySimpl
 
     private static void bufferColor(BufferBuilder buffer, int color) {
         buffer.color((color >>> 16) & 0xFF, (color >>> 8) & 0xFF, color & 0xFF, (color >>> 24) & 0xFF);
+    }
+
+    // Normal is towards +z same as pitch and yaw = 0
+    // Math taken from Vec3d
+    private static void bufferPos(BufferBuilder buffer, double x, double y, double z, float pitch, float yaw) {
+        double f, f1;
+
+        f = MathHelper.cos(pitch);
+        f1 = MathHelper.sin(pitch);
+        y = y * f + z * f1;
+        z = z * f - y * f1;
+
+        f = MathHelper.cos(yaw);
+        f1 = MathHelper.sin(yaw);
+        x = x * f + z * f1;
+        z = z * f - x * f1;
+
+        buffer.pos(x, y, z);
     }
 
 }
